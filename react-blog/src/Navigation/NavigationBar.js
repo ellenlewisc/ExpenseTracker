b24ac75dd179960207db49023c3dcc76a9c903fc
ellen <elewis@immediac.com>
2023-03-01 09:14:57 -0400

    navbar links and styling

--- react-blog/src/Navigation/NavigationBar.js
+++ react-blog/src/Navigation/NavigationBar.js
@@ -2,6 +2,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
@@ -9,13 +10,12 @@ function NavigationBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Link className="no-underline" to={"./"}><Navbar.Brand> Expense Tracker</Navbar.Brand></Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <Navbar.Text><Link className="no-underline p-10" to={"./About"}>About</Link></Navbar.Text>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
--- react-blog/src/Views/Home.js
+++ react-blog/src/Views/Home.js
@@ -1,8 +1,8 @@
 function Home(){
     return (
         <div className="container mx-auto">
-       <p className="text-3xl text-gray-700 font-bold mb-5 mt-10">
-        Exspenses
+       <p className="flex justify-center text-3xl text-gray-700 font-bold mb-5 mt-10">
+        Expenses
       </p>
     </div>
     )
